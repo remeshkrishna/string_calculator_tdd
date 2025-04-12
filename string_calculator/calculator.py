cef169536332ebf6678beac3958fa600d875d9a9
unknown <remeshkrishnaur@gmail.com>
2025-04-12 21:13:06 +0530

    testcase6: handle custom single character delimiter: passed

--- string_calculator/calculator.py
+++ string_calculator/calculator.py
@@ -1,10 +1,17 @@
 class Calculator:
-    def add(self,inputString):
+    def add(self,inputString:str):
         try:
             if inputString=="":
                 return 0
-            inputString = inputString.replace('\n',',')
-            res=sum(map(int,inputString.split(',')))
+            delimiter = ','
+            if inputString.startswith("//"):
+                parts = inputString.split('\n',1)
+                delimiter = parts[0].replace('//','')
+                inputString = parts[1]
+
+            inputString = inputString.replace('\n',delimiter)
+
+            res=sum(map(int,inputString.split(delimiter)))
             return res
         except ValueError:
             raise ValueError("Invalid string provided")
